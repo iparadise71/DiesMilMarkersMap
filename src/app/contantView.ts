b33c9cde6a42c7b2aaa51c4632af7697cf32fed4
iparadise71 <iparadise71@gmail.com>
2018-11-13 15:14:23 -0400

    imagen de muestra

--- src/app/contantView.ts
+++ src/app/contantView.ts
@@ -1,19 +1,19 @@
 export class CONSTANT {
 
     public static CODE_EVENT_FILTER = [
-        {icon: 'assets/img/m_mbp.png',  codeEventKey: 1,  colorEvent: '#ffff00', codeEventState: false  },
-        {icon: 'assets/img/m_parq.png',  codeEventKey: 3,  colorEvent: '#F06292', codeEventState: false },
-        {icon: 'assets/img/m_coac.png',  codeEventKey: 8,  colorEvent: '#A9B9BB', codeEventState: false },
-        {icon: 'assets/img/m_mev.png',  codeEventKey: 13,  colorEvent: '#A91818', codeEventState: false },
-        {icon: 'assets/img/m_meg.png',  codeEventKey: 14,  colorEvent: '#00AD9D', codeEventState: false },
-        {icon: 'assets/img/m_msg.png',  codeEventKey: 15,  colorEvent: '#E69A2A', codeEventState: false },
-        {icon: 'assets/img/m_mcbp.png',  codeEventKey: 16,  colorEvent: '#17C723', codeEventState: false },
-        {icon: 'assets/img/m_msbp.png',  codeEventKey: 17,  colorEvent: '#B675F3', codeEventState: false },
-        {icon: 'assets/img/m_mfh.png',  codeEventKey: 27,  colorEvent: '#2e2f2a', codeEventState: false },
-        {icon: 'assets/img/m_mar.png',  codeEventKey: 28,  colorEvent: '#785448', codeEventState: false },
-        {icon: 'assets/img/m_mpm.png',  codeEventKey: 31,  colorEvent: '#DFD195', codeEventState: false },
-        {icon: 'assets/img/m_mim.png',  codeEventKey: 32,  colorEvent: '#00ffff', codeEventState: false },
-        {icon: 'assets/img/m_mim.png',  codeEventKey: 33,  colorEvent: '#00ffff', codeEventState: false },
+        {icon: 'assets/img/m_mbp.png',  codeEventKey: 1,  colorEvent: '#ffff00', codeEventState: true  },
+        {icon: 'assets/img/m_parq.png',  codeEventKey: 3,  colorEvent: '#F06292', codeEventState: true },
+        {icon: 'assets/img/m_coac.png',  codeEventKey: 8,  colorEvent: '#A9B9BB', codeEventState: true },
+        {icon: 'assets/img/m_mev.png',  codeEventKey: 13,  colorEvent: '#A91818', codeEventState: true },
+        {icon: 'assets/img/m_meg.png',  codeEventKey: 14,  colorEvent: '#00AD9D', codeEventState: true },
+        {icon: 'assets/img/m_msg.png',  codeEventKey: 15,  colorEvent: '#E69A2A', codeEventState: true },
+        {icon: 'assets/img/m_mcbp.png',  codeEventKey: 16,  colorEvent: '#17C723', codeEventState: true },
+        {icon: 'assets/img/m_msbp.png',  codeEventKey: 17,  colorEvent: '#B675F3', codeEventState: true },
+        {icon: 'assets/img/m_mfh.png',  codeEventKey: 27,  colorEvent: '#2e2f2a', codeEventState: true },
+        {icon: 'assets/img/m_mar.png',  codeEventKey: 28,  colorEvent: '#785448', codeEventState: true },
+        {icon: 'assets/img/m_mpm.png',  codeEventKey: 31,  colorEvent: '#DFD195', codeEventState: true },
+        {icon: 'assets/img/m_mim.png',  codeEventKey: 32,  colorEvent: '#00ffff', codeEventState: true },
+        {icon: 'assets/img/m_mim.png',  codeEventKey: 33,  colorEvent: '#00ffff', codeEventState: true },
         {icon: 'assets/img/m_mnn.png',  codeEventKey: 30,  colorEvent: '#468c2e', codeEventState: true }
     ];
 }
